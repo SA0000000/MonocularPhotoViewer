23b06156b09908b772b5a8dad952a92cfaf8683b
SJA0000016 <sarahj.andrabi@gmail.com>
2014-12-08 01:37:31 -0500

    Added Zooming
    Can zoom in and out in x as well as y directions.
    Moving image isn't working

--- Viewer.xaml.cs
+++ Viewer.xaml.cs
@@ -22,8 +22,8 @@ namespace MonocularPhotoViewer
     public partial class Viewer : Window
     {
         DispatcherTimer _timer = new DispatcherTimer();     //to poll for Xbox controller events
-        TransformGroup xformGroupLeft, xformGroupRight;
-        ScaleTransform xformLeft, xformRight;
+        TransformGroup xformGroup;
+        ScaleTransform xform;
         GamePadState lastGamePadState;
 
         
@@ -42,13 +42,13 @@ namespace MonocularPhotoViewer
 
         //For zooming
         enum Zoom : int { zoomIn = 1, zoomOut = -1 };
-        int zoom = 0;
         float zoomValue = 0.05f;
+        int x_zoom = 0, y_zoom = 0;
 
         //create variables that will store where the user set the first image and 
         //then will be used to display the other images at the same spatial window coordinates and state
         bool setImagePosition = false;
-        double leftImg_leftPos, leftImg_topPos;
+        double leftPos, topPos;
         double[] img_zoomVal = new double[2];
 
         //create an instance of Class Images to store and deal with all images
@@ -60,21 +60,22 @@ namespace MonocularPhotoViewer
             LeftCanvas.Visibility = Visibility.Visible;
 
             //set transforms for left canvas
-            xformGroupLeft = new TransformGroup();
-            xformLeft = new ScaleTransform();
-            xformGroupLeft.Children.Add(xformLeft);
-            leftImage.RenderTransform = xformGroupLeft;
+            xformGroup = new TransformGroup();
+            xform = new ScaleTransform();
+            xformGroup.Children.Add(xform);
+            imageRegion.RenderTransform = xformGroup;
 
             //set initial zoom values for images
             //image
-            img_zoomVal[0] = xformLeft.ScaleX + 0.15f * (-1);
-            img_zoomVal[1] = xformLeft.ScaleY + 0.15f * (-1);
+            img_zoomVal[0] = xform.ScaleX + 0.15f * (-1);
+            img_zoomVal[1] = xform.ScaleY + 0.15f * (-1);
 
             //init Images class object
             images = new Images(studynum, filelist, training, task1);
 
             //set first image on the canvas
-            leftImage.Source = new BitmapImage(new Uri(@"FirstImage.png", UriKind.RelativeOrAbsolute));
+            imageRegion.Source = new BitmapImage(new Uri(@"FirstImage.png", UriKind.RelativeOrAbsolute));
+            imageRegion.Focus();
 
             //set timer object
             _timer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(100) };
@@ -103,35 +104,34 @@ namespace MonocularPhotoViewer
                 //if DPad has been pressed...check which direction and move the image accordingly
                 if (currentState.DPad.Up == ButtonState.Pressed)
                 {
-                    move = (int)Direction.up;
+                    y_zoom = (int)Zoom.zoomOut;
                 }
                 else if (currentState.DPad.Down == ButtonState.Pressed)
                 {
-                    move = (int)Direction.down;
+                    y_zoom = (int)Zoom.zoomIn;
                 }
                 else if (currentState.DPad.Left == ButtonState.Pressed)
                 {
-                    move = (int)Direction.left;
+                    x_zoom = (int)Zoom.zoomOut;
                 }
                 else if (currentState.DPad.Right == ButtonState.Pressed)
                 {
-                    move = (int)Direction.right;
+                    x_zoom = (int)Zoom.zoomIn;
                 }
-                else
-                    move = 0;
                 #endregion
 
                 #region LeftThumbstick Direction
-                //ifLeftThumbstick has been moved up or down zoom in and out respectively
+                //ifLeftThumbstick has been moved move image accordingly
                 if (lastGamePadState.IsButtonUp(Buttons.LeftThumbstickUp) && currentState.IsButtonDown(Buttons.LeftThumbstickUp))
-                {
-                    zoom = (int)Zoom.zoomOut;
-                }
-
+                    move = (int)Direction.up;
                 else if (lastGamePadState.IsButtonUp(Buttons.LeftThumbstickDown) && currentState.IsButtonDown(Buttons.LeftThumbstickDown))
-                {
-                    zoom = (int)Zoom.zoomIn;
-                }
+                    move = (int)Direction.down;
+                else if (lastGamePadState.IsButtonUp(Buttons.LeftThumbstickRight) && currentState.IsButtonDown(Buttons.LeftThumbstickRight))
+                    move = (int)Direction.up;
+                else if (lastGamePadState.IsButtonUp(Buttons.LeftThumbstickLeft) && currentState.IsButtonDown(Buttons.LeftThumbstickLeft))
+                    move = (int)Direction.left;
+                else
+                    move = 0;
                 #endregion
 
                 //if Button A has been pressed move to the next image and save state accordingly
@@ -151,23 +151,23 @@ namespace MonocularPhotoViewer
             //update position of image
             //get focus on image
                 LeftCanvas.Focus();
-                Double LeftPos = Convert.ToDouble(leftImage.GetValue(Canvas.LeftProperty));
-                Double topPos = Convert.ToDouble(leftImage.GetValue(Canvas.TopProperty));
+                Double LeftPos = Convert.ToDouble(imageRegion.GetValue(Canvas.LeftProperty));
+                Double TopPos = Convert.ToDouble(imageRegion.GetValue(Canvas.TopProperty));
                 if (move != 0)
-                    moveImage(LeftPos, topPos, leftImage);
-                if (zoom != 0)
+                    moveImage(LeftPos, TopPos, imageRegion);
+                if (x_zoom != 0 || y_zoom !=0)
                 {
-                    zoomImage(LeftPos, topPos, leftImage);
+                    zoomImage(LeftPos, TopPos, imageRegion);
                 }
 
                 //store the values where the left image should be drawn and what their zoom value should be
                 //so that all other subsequent images with the same values
-                leftImg_leftPos = LeftPos;
-                leftImg_topPos = topPos; 
+                leftPos = LeftPos;
+                topPos = TopPos; 
         }
 
         //move the image in 2D
-        void moveImage(double LeftPos, double topPos, Image image)
+        void moveImage(double LeftPos, double TopPos, Image image)
         {
             if (move == (int)Direction.left)
             {
@@ -183,16 +183,16 @@ namespace MonocularPhotoViewer
             else if (move == (int)Direction.up)
             {
                 //if (topPos < (LeftCanvas.ActualHeight - leftImage.Height))
-                image.SetValue(Canvas.TopProperty, topPos - moveValue);
+                image.SetValue(Canvas.TopProperty, TopPos - moveValue);
             }
             else if (move == (int)Direction.down)
             {
-                image.SetValue(Canvas.TopProperty, topPos + moveValue);
+                image.SetValue(Canvas.TopProperty, TopPos + moveValue);
             }
             else if (move == -1)
             {
                 //image needs to be redrawn at topPos, leftPos because it was scaled 
-                image.SetValue(Canvas.TopProperty, topPos);
+                image.SetValue(Canvas.TopProperty, TopPos);
                 image.SetValue(Canvas.LeftProperty, LeftPos);
             }
 
@@ -201,42 +201,43 @@ namespace MonocularPhotoViewer
         }
 
         //make image larger or smaller
-        void zoomImage(double LeftPos, double topPos, Image image)
+        void zoomImage(double LeftPos, double TopPos, Image image)
         {
             TransformGroup transformGroup = (TransformGroup)image.RenderTransform;
             ScaleTransform transform = (ScaleTransform)transformGroup.Children[0];
 
             //scale image and then redraw it at the previous position
             //only scale if scaling factor is greater than zero
-            if (transform.ScaleX + zoomValue * zoom > 0)
+            if (transform.ScaleX + zoomValue * x_zoom > 0 || transform.ScaleY + zoomValue * y_zoom > 0)
             {
-                transform.ScaleX += zoomValue * zoom;
-                transform.ScaleY += zoomValue * zoom;
+                transform.ScaleX += zoomValue * x_zoom;
+                transform.ScaleY += zoomValue * y_zoom;
                 img_zoomVal[0] = transform.ScaleX;
                 img_zoomVal[1] = transform.ScaleY;
             }
 
             //after zooming make sure the top left corner of the image doesn't change
             move = -1;
-            moveImage(LeftPos, topPos, image);
+            moveImage(LeftPos, TopPos, image);
 
             //reset zoom to 0
-            zoom = 0;
+            x_zoom = 0;
+            y_zoom = 0;
         }
 
         //move to the next image
         void NextImage()
         {
             String uri = images.nextImage();
-            leftImage.Source = new BitmapImage(new Uri(uri, UriKind.RelativeOrAbsolute));
+            imageRegion.Source = new BitmapImage(new Uri(uri, UriKind.RelativeOrAbsolute));
 
             //draw the image at the place where the user last set the location to
-            moveImage(leftImg_leftPos, leftImg_topPos, leftImage);
+            moveImage(leftPos, topPos, imageRegion);
             
             //set default zoom values for images
             //left image
-            xformLeft.ScaleX = xformRight.ScaleX = img_zoomVal[0];
-            xformLeft.ScaleY = xformRight.ScaleY = img_zoomVal[1];
+            xform.ScaleX = img_zoomVal[0];
+            xform.ScaleY = img_zoomVal[1];
 
             //check if the user has reached the last image 
             //if yes then inform them they are done
